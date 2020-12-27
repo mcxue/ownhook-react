b71d6f9e035073cdda67d79771801aea98569282
EddieMa <machaoxue@yeah.net>
2020-12-27 20:18:05 +0800

    deploy

---
+++ precache-manifest.801b1f68cc61357078d6567499270ef6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cbe00984831eaf49d87a817f49db8fa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e0738dfaab0a969bda36",
+    "url": "./static/css/main.81acfe78.chunk.css"
+  },
+  {
+    "revision": "34e459ea1f0fd0812ecc",
+    "url": "./static/js/2.ae57abc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ae57abc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0738dfaab0a969bda36",
+    "url": "./static/js/main.84993543.chunk.js"
+  },
+  {
+    "revision": "1b44015d6c10bce66bed",
+    "url": "./static/js/runtime-main.b92c262e.js"
+  }
+]);
